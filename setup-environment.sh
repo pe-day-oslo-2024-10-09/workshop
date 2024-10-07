df917c7738f67ce31f74cd20c7f8a59a09a1e7ed
Yves Hwang <yveshwang@gmail.com>
2024-10-07 11:58:11 +0200

    fix: using AWS managed SecretsManager policy, and setting org after retreiving it

--- setup-environment.sh
+++ setup-environment.sh
@@ -2,12 +2,15 @@
 
 echo "Configuring default Humanitec Org"
 
-PE_DAY_ORG="$(humctl get orgs -o json | jq -r '[.[] | select(.metadata.id | startswith("pe-day-osl^C")) | .metadata.id][0]')"
+PE_DAY_ORG="$(humctl get orgs -o json | jq -r '[.[] | select(.metadata.id | startswith("pe-day-osl")) | .metadata.id][0]')"
 if [ "$PE_DAY_ORG" == "null" ]
 then
   echo "Could not find Platform Engineering Day organisation. Have you accepted your invite?"
   exit 1
 fi
+
+echo "    Org: $PE_DAY_ORG"
+humctl config set org $PE_DAY_ORG
 
 echo "Confoguring kubectl context"
 aws eks update-kubeconfig --region us-west-2 --name eks-workshop
@@ -18,7 +21,9 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/cont
 
 echo "Configuring IAM for SecretManager access"
 AWS_NODE_ROLE="$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | startswith("eksctl-eks-workshop-nodegroup-defa-NodeInstanceRole")) | .RoleName')"
-AWS_SECRET_MANAGER_POLICY="$(aws iam list-policies | jq -r '.Policies[] | select(.PolicyName | startswith("secrets-manager-access-")) | .Arn')"
+echo "    Role:  $AWS_NODE_ROLE"
+AWS_SECRET_MANAGER_POLICY="$(aws iam list-policies | jq -r '.Policies[] | select(.PolicyName | startswith("SecretsManagerReadWrite")) | .Arn')"
+echo "    Policy: $AWS_SECRET_MANAGER_POLICY"
 
 aws iam attach-role-policy --role-name "${AWS_NODE_ROLE}" --policy-arn "${AWS_SECRET_MANAGER_POLICY}"
 
